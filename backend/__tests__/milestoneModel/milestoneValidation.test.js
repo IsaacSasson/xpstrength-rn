b12aac2964cb0279d2b4a545be69c437d762da80
(Buffer)&Overbyte <88282354+Reuvi@users.noreply.github.com>
2025-06-18 20:08:23 -0400

    validation testing

---
+++ backend/__tests__/milestoneModel/milestoneValidation.test.js
@@ -0,0 +1,66 @@
+import User from "../../models/user.model.js";
+import Milestone from "../../models/milestone.model.js";
+
+describe.skip("Milestone Validation Checks", () => {
+    it("Stops Duplicates", async () => {
+        const user = await User.create({
+            username: "testUser",
+            password: "StrongPass12!",
+            email: "test@gmail.com"
+        });
+
+        const milestoneData = await Milestone.findOne({ where: { userId: user.id } });
+
+        milestoneData.milestones = [1, 2, 1];
+        expect(milestoneData.save()).rejects.toThrow();
+
+        user.destroy();
+    });
+
+    it("Forces Ids to be a number", async () => {
+        const user = await User.create({
+            username: "testUser",
+            password: "StrongPass12!",
+            email: "test@gmail.com"
+        });
+
+        const milestoneData = await Milestone.findOne({ where: { userId: user.id } });
+
+        milestoneData.milestones = [1, "2"];
+        expect(milestoneData.save()).rejects.toThrow();
+
+        user.destroy();
+    });
+
+    it("Milestone must exist in global referecne", async () => {
+        const user = await User.create({
+            username: "testUser",
+            password: "StrongPass12!",
+            email: "test@gmail.com"
+        });
+
+        const milestoneData = await Milestone.findOne({ where: { userId: user.id } });
+
+        milestoneData.milestones = [1, 10, -1];
+        expect(milestoneData.save()).rejects.toThrow();
+
+        user.destroy();
+    });
+
+    it("Milestone succesfully updates", async () => {
+        const user = await User.create({
+            username: "testUser",
+            password: "StrongPass12!",
+            email: "test@gmail.com"
+        });
+
+        const milestoneData = await Milestone.findOne({ where: { userId: user.id } });
+
+        milestoneData.milestones = [1, 2, 3];
+        await expect(milestoneData.save())
+            .resolves
+            .toHaveProperty("milestones", [1, 2, 3]);
+
+        user.destroy();
+    });
+})
