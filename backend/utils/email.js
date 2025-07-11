8d7f92c501fbf74b070702b1c7378c01911d8bde
(Buffer)&Overbyte <88282354+Reuvi@users.noreply.github.com>
2025-07-11 19:11:32 -0400

    updated email

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,71 +1,69 @@
-import nodemailer from 'nodemailer';
-import pug from 'pug';
-import { htmlToText } from 'html-to-text'
-import path from 'path'
-import { fileURLToPath } from 'url';
+import nodemailer from "nodemailer";
+import pug from "pug";
+import { htmlToText } from "html-to-text";
+import path from "path";
+import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-
 export class Email {
-    constructor(user, url) {
-        this.to = user.email;
-        this.username = user.username;
-        this.url = url;
-        this.from = `XPStrength Support <${process.env.EMAIL_FROM}>`
+  constructor(user, url) {
+    this.to = user.email;
+    this.username = user.username;
+    this.url = url;
+    this.from = `XPStrength Support <${process.env.EMAIL_FROM}>`;
+  }
+
+  async newTransport() {
+    if (process.env.NODE_ENV === "production") {
+      //sendGrid
+      return 1;
     }
 
-    async newTransport() {
-        if (process.env.NODE_ENV === 'production') {
-            //sendGrid
-            return 1;
-        }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
 
-        return nodemailer.createTransport({
-            host: process.env.EMAIL_HOST,
-            port: process.env.EMAIL_PORT,
-            auth: {
-                user: process.env.EMAIL_USERNAME,
-                pass: process.env.EMAIL_PASSWORD
-            }
-        })
-    }
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      name: this.username,
+      url: this.url,
+      subject,
+    });
 
-    async send(template, subject) {
-        // 1) Render HTML based on a pug template
-        const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
-            name: this.username,
-            url: this.url,
-            subject
-        })
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText(html),
+    };
 
-        const mailOptions = {
-            from: this.from,
-            to: this.to,
-            subject,
-            html,
-            text: htmlToText(html)
-        }
+    const tranport = await this.newTransport();
 
-        const tranport = await this.newTransport();
+    await tranport.sendMail(mailOptions);
+  }
 
-        await tranport.sendMail(mailOptions)
+  async sendWelcome() {
+    await this.send("welcome", "Welcome to XPStrength");
+  }
 
+  async sendForgotPassword() {
+    return this.send(
+      "forgotPassword",
+      "Your password reset token (valid for 5 min)"
+    );
+  }
 
-    }
-
-    async sendWelcome() {
-        await this.send('welcome', "Welcome to XPStrength")
-    }
-
-    async sendForgotPassword() {
-        return this.send('forgotPassword', "Your password reset token (valid for 15 min)")
-    }
-
-    async sendForgotUsername() {
-
-        return this.send('forgotUsername', "Username Request")
-    }
+  async sendForgotUsername() {
+    return this.send("forgotUsername", "Username Request");
+  }
 }
 
-
-export default Email
+export default Email;
