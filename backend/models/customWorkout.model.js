f5a8cd32177d6fe49af8acf9af42b08fd2618fa8
(Buffer)&Overbyte <88282354+Reuvi@users.noreply.github.com>
2025-08-04 18:33:09 -0400

    Custom Workout name length reduced by half (max 40 Characters)

--- backend/models/customWorkout.model.js
+++ backend/models/customWorkout.model.js
@@ -4,39 +4,51 @@ import { checkCustomWorkoutFormat } from "../validators/customWorkout/checkCusto
 import { sequelize } from "../config/db.config.js";
 
 const CustomWorkout = sequelize.define(
-    "customWorkouts",
-    {
-        id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true, unique: true },
-        userId: {
-            type: DataTypes.INTEGER, allowNull: false, references: {
-                model: "Users",
-                key: "id"
-            },
-            onDelete: 'CASCADE'
-        },
-        name: {
-            type: DataTypes.STRING, allowNull: false, unique: false, validate: {
-                min: 3,
-                max: 80,
-                isTextClean,
-            },
-            comment: "The name of the created custom workout."
-        },
-        exercises: {
-            type: DataTypes.JSON, allowNull: false, defaultValue: [], validate: {
-                checkCustomWorkoutFormat
-            },
-            comment: "Array of inOrder exercise objects to perform for workout."
-        },
+  "customWorkouts",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      unique: true,
     },
-    {
-        tableName: "customWorkouts",
-        timestamps: true,
-        underscored: true,
-        indexes: [
-            { fields: ['user_id'] },
-        ],
-    }
-)
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Users",
+        key: "id",
+      },
+      onDelete: "CASCADE",
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
+      validate: {
+        min: 3,
+        max: 40,
+        isTextClean,
+      },
+      comment: "The name of the created custom workout.",
+    },
+    exercises: {
+      type: DataTypes.JSON,
+      allowNull: false,
+      defaultValue: [],
+      validate: {
+        checkCustomWorkoutFormat,
+      },
+      comment: "Array of inOrder exercise objects to perform for workout.",
+    },
+  },
+  {
+    tableName: "customWorkouts",
+    timestamps: true,
+    underscored: true,
+    indexes: [{ fields: ["user_id"] }],
+  }
+);
 
-export default CustomWorkout
+export default CustomWorkout;
