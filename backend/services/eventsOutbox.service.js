4b220a5bcb669fefc4d9697e5a124b5644801053
(Buffer)&Overbyte <88282354+Reuvi@users.noreply.github.com>
2025-08-12 20:13:36 -0400

    [1] Completed eventOutbox Service, [2] Created WebSocket Testing endpoint in public/index.html

--- backend/services/eventsOutbox.service.js
+++ backend/services/eventsOutbox.service.js
@@ -1,7 +1,93 @@
-//Used for Sending events back to users through the Websockets and handling DB Seens
+import { sequelize } from "../config/db.config.js";
+import { Event } from "../models/index.js";
+import AppError from "../utils/AppError.js";
+import mapSequelizeError from "../utils/mapSequelizeError.js";
+import AddEvent from "../utils/AddEvent.js";
 
-//Will have one function for getting all events for a user
+//Singular event sent to user
+export function sendEvent(event, socket) {
+  socket.emit("newEvent", event);
+}
 
-//Another function for Marking off seen events for a user
+//Multiple events sent to user via an Array of events
+export function sendEvents(events, socket) {
+  socket.emit("newEvents", events);
+}
 
-//And will have another function for creating events for users from other users and will emit them out (to be done in a promise.all)
+export async function createEvent(
+  userId,
+  type,
+  actorId,
+  resourceId,
+  payload,
+  socket
+) {
+  try {
+    await sequelize.transaction(async (t) => {
+      let newEvent = new AddEvent(userId, type, actorId, resourceId, payload);
+      newEvent = await newEvent.forward(t);
+
+      const plain = newEvent.get ? newEvent.get({ plain: true }) : newEvent;
+      t.afterCommit(() => {
+        try {
+          sendEvent(plain, socket);
+        } catch (err) {
+          throw new AppError(
+            "Failed to event back through socket",
+            500,
+            "WEBSOCKET"
+          );
+        }
+      });
+    });
+  } catch (err) {
+    throw mapSequelizeError(err);
+  }
+}
+
+export async function markEventsSeen(upToId, userId, socket) {
+  try {
+    await sequelize.transaction(async (t) => {
+      await Event.markSeen(userId, upToId, t);
+    });
+    socket.emit("eventsSeenUpToId", { upTo: upToId });
+  } catch (err) {
+    throw mapSequelizeError(err);
+  }
+}
+
+export async function getAllUnseenEvents(userId, socket) {
+  try {
+    await sequelize.transaction(async (t) => {
+      const events = await Event.findAll({
+        where: { userId: userId, seenAt: null },
+        transaction: t,
+        order: [["id", "ASC"]],
+      });
+
+      const plain = events.map((e) => (e.get ? e.get({ plain: true }) : e));
+
+      t.afterCommit(() => {
+        try {
+          sendEvents(plain, socket);
+        } catch (err) {
+          throw new AppError(
+            "Failed to events back through socket",
+            500,
+            "WEBSOCKET"
+          );
+        }
+      });
+    });
+  } catch (err) {
+    throw mapSequelizeError(err);
+  }
+}
+
+export default {
+  getAllUnseenEvents,
+  markEventsSeen,
+  createEvent,
+  sendEvent,
+  sendEvents,
+};
