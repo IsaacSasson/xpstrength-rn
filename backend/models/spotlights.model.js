1aad512fa1824c3f9179d3dc3bb0c24634bb163e
(Buffer)&Overbyte <88282354+Reuvi@users.noreply.github.com>
2025-08-20 21:44:29 -0400

    Along with prev commit

---
+++ backend/models/spotlights.model.js
@@ -0,0 +1,48 @@
+import { Sequelize, DataTypes } from "sequelize";
+import { sequelize } from "../config/db.config.js";
+import spotLightTypes from "../../shared/spotlightTypes.json" with {type: 'json'};
+
+const Spotlight = sequelize.define(
+  "Spotlight",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Users",
+        key: "id",
+      },
+      onDelete: "CASCADE",
+    },
+    type: {
+      type: DataTypes.STRING(40),
+      allowNull: false,
+      validate: {
+        isIn: [spotLightTypes]
+      },
+    },
+    references: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    payload: {
+      type: DataTypes.JSON,
+      allowNull: true
+    }
+  },
+  {
+    tableName: "Spotlight",
+    timestamps: true,
+    underscored: true,
+    indexes: [{ fields: ["user_id"] }],
+  }
+);
+
+export default Spotlight;
