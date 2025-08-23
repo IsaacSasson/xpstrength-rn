e2fae0f5db273e31b46439d4aaaa7101212a889e
(Buffer)&Overbyte <88282354+Reuvi@users.noreply.github.com>
2025-08-23 12:18:23 -0400

    Spotlight service complete without event emitting

--- backend/services/spotlight.service.js
+++ backend/services/spotlight.service.js
@@ -1,10 +1,11 @@
 import mapSequelizeError from "../utils/mapSequelizeError.js";
 import AppError from "../utils/AppError.js";
-import { Spotlight, User, PersonalBest } from "../models/index.js";
+import { Spotlight, User, PersonalBest, Milestone } from "../models/index.js";
 import { sequelize } from "../config/db.config.js";
 import AddHistory from "../utils/AddHistory.js";
 import progressService from "./progress.service.js";
 
+//Complete, fetches all possible spotlights a user has
 export async function getSpotlights(user) {
   try {
     const spotlights = await Spotlight.findAll({
@@ -21,7 +22,42 @@ export async function getSpotlights(user) {
         } else if (type === "personalGoal") {
           payload = await progressService.updateGoal(user, spotlight.goalId);
         } else if (type === "milestone") {
-          //Update when you do Milestones
+          payload = spotlight.payload;
+          return;
+        }
+        return {
+          type: spotlight.type,
+          id: spotlight.id,
+          display: payload,
+          equipped: spotlight.equipped,
+        };
+      })
+    );
+
+    return result;
+  } catch (err) {
+    throw mapSequelizeError(err);
+  }
+}
+
+//For websocket access only by server never allowed from user
+export async function getEquippedSpotlights(userId) {
+  try {
+    const spotlights = await Spotlight.findAll({
+      where: { userId, equipped: true },
+      raw: true,
+    });
+
+    const result = await Promise.all(
+      spotlights.map(async (spotlight) => {
+        const type = spotlight.type;
+        let payload = "Failed to retrieve";
+        if (type === "exercise") {
+          payload = spotlight.payload;
+        } else if (type === "personalGoal") {
+          payload = await progressService.updateGoal(userId, spotlight.goalId);
+        } else if (type === "milestone") {
+          payload = spotlight.payload;
           return;
         }
         return {
@@ -56,19 +92,20 @@ export async function createSpotlight(user, referenceIds, type, autoEquip) {
         userId: user.id,
         type: type,
         workoutId: referenceIds[1],
+        payload: { exerciseId: referenceIds[0], PR: exercise },
         equipped: equipped,
       });
 
       result = {
         id: newSpotlight.id,
         type: newSpotlight.type,
-        display: exercise,
+        display: newSpotlight.payload,
         equipped: newSpotlight.equipped,
       };
     } else if (type === "personalGoal") {
       //referenceID represents an ID of the usersGoal index 0 is the id of goal
       const userGoal = await progressService.updateGoal(user, referenceIds[0]);
-      const newSpotlight = await Spotlight.create({
+      newSpotlight = await Spotlight.create({
         userId: user.id,
         type: type,
         goalId: referenceIds[0],
@@ -83,9 +120,40 @@ export async function createSpotlight(user, referenceIds, type, autoEquip) {
       };
     } else if (type === "milestone") {
       //referenceID represents the unique id of the milestone
+      const mileStone = await Milestone.findOne({
+        where: {
+          userId: user.id,
+          id: referenceIds[0],
+          attributes: ["milestone, createdAt"],
+          raw: true,
+        },
+      });
 
-      //Update when you do milestones
-      return;
+      if (!mileStone) {
+        throw new AppError(
+          "User does not have that milestone completed!",
+          400,
+          "BAD_DATA"
+        );
+      }
+
+      newSpotlight = await Spotlight.create({
+        userId: user.id,
+        type: type,
+        milestoneId: referenceIds[0],
+        payload: {
+          milestone: mileStone["milestone"],
+          timestamp: mileStone["createdAt"],
+        },
+        equipped: equipped,
+      });
+
+      result = {
+        id: newSpotlight.id,
+        type: newSpotlight.type,
+        display: newSpotlight,
+        equipped: newSpotlight.equipped,
+      };
     } else {
       throw new AppError(
         "Unknown type provided to create spotlight",
@@ -95,7 +163,7 @@ export async function createSpotlight(user, referenceIds, type, autoEquip) {
     }
 
     if (autoEquip) {
-      //Call Update Profile for other users
+      console.log("Emitting Equipped");
     }
     return result;
   } catch (err) {
@@ -105,6 +173,21 @@ export async function createSpotlight(user, referenceIds, type, autoEquip) {
 
 export async function deleteSpotlight(user, spotlightId) {
   try {
+    const spotlight = await Spotlight.findOne({
+      where: { userId: user.id, id: spotlightId },
+    });
+
+    if (!spotlight) {
+      throw new AppError("Spotlight not found to delete", 400, "BAD_DATA");
+    }
+
+    if (spotlight.equipped) {
+      console.log("Emitting Spotlight removed from equipped");
+    }
+
+    await spotlight.destroy();
+
+    return;
   } catch (err) {
     throw mapSequelizeError(err);
   }
@@ -112,6 +195,24 @@ export async function deleteSpotlight(user, spotlightId) {
 
 export async function equipSpotlight(user, spotlightId) {
   try {
+    const spotlight = await Spotlight.findOne({
+      where: { userId: user.id, id: spotlightId },
+    });
+
+    if (!spotlight) {
+      throw new AppError("Spotlight not found to delete", 400, "BAD_DATA");
+    }
+
+    if (spotlight.equipped) {
+      throw new AppError("Spotlight is already equipped");
+    }
+
+    spotlight.equipped = true;
+    spotlight.save();
+
+    console.log("Emitting Equipped");
+
+    return;
   } catch (err) {
     throw mapSequelizeError(err);
   }
@@ -119,6 +220,24 @@ export async function equipSpotlight(user, spotlightId) {
 
 export async function unequipSpotlight(user, spotlightId) {
   try {
+    const spotlight = await Spotlight.findOne({
+      where: { userId: user.id, id: spotlightId },
+    });
+
+    if (!spotlight) {
+      throw new AppError("Spotlight not found to delete", 400, "BAD_DATA");
+    }
+
+    if (!spotlight.equipped) {
+      throw new AppError("Spotlight is already unequipped", 400, "BAD_DATA");
+    }
+
+    spotlight.equipped = false;
+    spotlight.save();
+
+    console.log("Emitting unequip");
+
+    return;
   } catch (err) {
     throw mapSequelizeError(err);
   }
